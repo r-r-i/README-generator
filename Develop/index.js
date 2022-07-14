aecd689c54466ab5cff4820e69766e3722fc8bb9
brake <riaconoo@icloud.com>
2022-07-14 13:52:36 +1000

    initial commit with user questions

---
+++ Develop/index.js
@@ -0,0 +1,61 @@
+// TODO: Include packages needed for this application
+
+const inquirer = require("inquirer");
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'project title',
+            name: 'title',
+        },
+        {
+            type: 'input',
+            message: 'description',
+            name: 'description',
+        },
+        {
+            type: 'input',
+            message: 'installation instructions',
+            name: 'install',
+        },
+        {
+            type: 'input',
+            message: 'usage information',
+            name: 'usage',
+        },
+        {
+            type: 'input',
+            message: 'contribution guidelines',
+            name: 'contributions',
+        },
+        {
+            type: 'input',
+            message: 'test instructions',
+            name: 'testing',
+        },
+        // Need to create a license choice option
+        {
+            type: 'input',
+            message: 'github',
+            name: 'github',
+        },
+        {
+            type: 'input',
+            message: 'email',
+            name: 'email',
+        },
+        
+    ])
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
